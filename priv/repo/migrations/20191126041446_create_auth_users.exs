27f99cab63c11ac0e7d46b8e4aca92d24698d3a3
Braden Douglass <braden.douglass@gmail.com>
2019-11-25 23:16:53 -0500

    Generate Authentication Context.
    The Auth Context will be used to encapsulate the authentication within
    the application. This is auto generated code using the phx.context mix
    task.

---
+++ priv/repo/migrations/20191126041446_create_auth_users.exs
@@ -0,0 +1,12 @@
+defmodule CockroachLiveview.Repo.Migrations.CreateAuthUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:auth_users) do
+      add :nickname, :string
+
+      timestamps()
+    end
+
+  end
+end
