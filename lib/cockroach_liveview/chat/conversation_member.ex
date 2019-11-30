137c6b28c96314d11f9df29d254715eb7189ea03
Braden Douglass <braden.douglass@gmail.com>
2019-11-30 16:36:39 -0500

    Define DB Constraints Around Conversation Member.
    To keep things unique across messages, define some unique constraints
    across the chat_conversation_members table.

--- lib/cockroach_liveview/chat/conversation_member.ex
+++ lib/cockroach_liveview/chat/conversation_member.ex
@@ -1,11 +1,13 @@
 defmodule CockroachLiveview.Chat.ConversationMember do
+  alias CockroachLiveview.Auth.User
+  alias CockroachLiveview.Chat.Conversation
   use Ecto.Schema
   import Ecto.Changeset
 
   schema "chat_conversation_members" do
     field :owner, :boolean, default: false
-    field :conversation_id, :id
-    field :user_id, :id
+    belongs_to :conversation, Conversation
+    belongs_to :user, User
 
     timestamps()
   end
@@ -13,7 +15,9 @@ defmodule CockroachLiveview.Chat.ConversationMember do
   @doc false
   def changeset(conversation_member, attrs) do
     conversation_member
-    |> cast(attrs, [:owner])
-    |> validate_required([:owner])
+    |> cast(attrs, [:owner, :conversation_id, :user_id])
+    |> validate_required([:owner, :conversation_id, :user_id])
+    |> unique_constraint(:user, name: :chat_conversation_members_conversation_id_user_id_index)
+    |> unique_constraint(:conversation_id, name: :chat_conversation_members_owner)
   end
 end
