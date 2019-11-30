a703f3e9d6a2cf650593d213a8725bb4c3c9ac66
Braden Douglass <braden.douglass@gmail.com>
2019-11-30 17:27:24 -0500

    Validations / Required Fields To Seen Messages.
    Create some required validations around the message and user id
    surrounding the seen message. Including some helper functions with
    belongs_to.

--- lib/cockroach_liveview/chat/seen_message.ex
+++ lib/cockroach_liveview/chat/seen_message.ex
@@ -1,10 +1,12 @@
 defmodule CockroachLiveview.Chat.SeenMessage do
+  alias CockroachLiveview.Auth.User
+  alias CockroachLiveview.Chat.Message
   use Ecto.Schema
   import Ecto.Changeset
 
   schema "chat_seen_messages" do
-    field :user_id, :id
-    field :message_id, :id
+    belongs_to :user, User
+    belongs_to :message, Message
 
     timestamps()
   end
@@ -12,7 +14,7 @@ defmodule CockroachLiveview.Chat.SeenMessage do
   @doc false
   def changeset(seen_message, attrs) do
     seen_message
-    |> cast(attrs, [])
-    |> validate_required([])
+    |> cast(attrs, [:user_id, :message_id])
+    |> validate_required([:user_id, :message_id])
   end
 end
